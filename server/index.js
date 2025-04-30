517a5de05a374b9e13725e5a2b23d5b205ab7437
Nevil koladiya <nevilkoladiya@gmail.com>
2025-05-01 00:09:15 +0530

    Update CORS origin to Vercel URL

--- server/index.js
+++ server/index.js
@@ -31,8 +31,6 @@ app.use(
   cors({
     // origin: "http://localhost:3000",
     origin: "*",
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
   })
 );
 
