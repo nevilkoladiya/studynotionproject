5ecf74dc47abb1d273b8c85cccd7c5e335ea747c
Nevil koladiya <nevilkoladiya@gmail.com>
2025-04-30 23:39:23 +0530

    Update CORS origin to Vercel URL

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "*",
+    origin: "https://studynotionproject-hb5kw303s-nevil-koladiyas-projects.vercel.app/",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
   })
